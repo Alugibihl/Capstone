f8f5372fe621c49d231e98ecf718a38407e93875
alexl <alexandermlugbiihl@gmail.com>
2023-05-16 13:19:46 -0400

    category model added and recipe model updated

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -11,14 +11,14 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     details = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     image = db.Column(db.String, nullable=False)
-    category_id = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id")), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
-    # category = db.relationship("Category", back_populates="recipes")
-    # user = db.relationship("User", back_populates="recipes")
+    category = db.relationship("Category", back_populates="recipes")
+    user = db.relationship("User", back_populates="recipes")
 
 
     def to_dict(self):
