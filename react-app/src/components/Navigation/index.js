180cfb2d4df114f4c7536d59a41ca74eff602eb9
alexl <alexandermlugbiihl@gmail.com>
2023-05-21 18:13:35 -0400

    background color and button styling

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -26,7 +26,8 @@ function Navigation({ isLoaded }) {
 			<div className='navbar'>
 				<div className='today'>{date}
 				</div>
-				<h1 className='title'><NavLink to="/">The New Fork Dines</NavLink></h1>
+				<h1 className='title'><NavLink to="/"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwebstockreview.net%2Fimages%2Ffork-clipart-fork-knife-12.png&f=1&nofb=1&ipt=a18a60daf24bbd79dd6ed07f3c7f8e3e931cc014b3ef3dea3f671be28d2050ed&ipo=images" alt="knife and fork">
+				</img>  The New Fork Dines  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwebstockreview.net%2Fimages%2Ffork-clipart-fork-knife-12.png&f=1&nofb=1&ipt=a18a60daf24bbd79dd6ed07f3c7f8e3e931cc014b3ef3dea3f671be28d2050ed&ipo=images" alt="knife and fork"></img></NavLink></h1>
 				{sessionUser === null ? <div className='title-buttons'><button onClick={loginRoute} className='blue-button'>Login</button><button onClick={signupRoute} className='blue-button' >Sign Up</button></div> : null}
 
 				{isLoaded && (
