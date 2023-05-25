7107b56cf6b68ad14a448c3c64e444ea7eb7b169
alexl <alexandermlugbiihl@gmail.com>
2023-05-25 17:41:39 -0400

    3rd crud done

--- react-app/src/components/Comments/getComments/index.js
+++ react-app/src/components/Comments/getComments/index.js
@@ -37,44 +37,47 @@ const CommentsByRecipe = ({ recipe }) => {
     if (!allUsers) return null
     return (
         <div className="comments-modal-container wrap-break">
-            <div className="comments-modal-text">
-                <div className="comment-page-title">
-                    <h1>Comments {recipeComments.length}</h1>
-                    <button onClick={closeModal}> X </button>
-                </div>
-                <h2>Please keep your comments respectful</h2>
-                {!editFormStatus[selectedCommentId] && (
-                    <div>
-                        <CreateCommentForm recipe={recipe} />
+            <div className="border-base-comments">
+                <div className="comments-modal-text">
+                    <div className="comment-page-title">
+                        <h1>Comments {recipeComments.length}</h1>
+                        <button onClick={closeModal}> X </button>
                     </div>
-                )}
-                <div>
-                    {recipeComments.map((comment) => {
-                        return (
-                            <div key={comment.id}>
-                                <OneComment
-                                    comment={comment}
-                                    recipe={recipe}
-                                    allUsers={allUsers}
-                                    editVisibility={editVisibility}
-                                    deleteVisible={deleteVisible}
-                                    setDeleteVisible={setDeleteVisible}
-                                    editFormStatus={editFormStatus}
-                                />
-                                {user.id === comment.userId && (
-                                    <div>
+                    <h2>Please keep your comments respectful</h2>
+                    {!editFormStatus[selectedCommentId] ? (
+                        <div>
+                            <CreateCommentForm recipe={recipe} />
+                        </div>
+                    ) : (<div className="filler-space"> </div>)}
+
+                    <div>
+                        {recipeComments.map((comment) => {
+                            return (
+                                <div key={comment.id}>
+                                    <OneComment
+                                        comment={comment}
+                                        recipe={recipe}
+                                        allUsers={allUsers}
+                                        editVisibility={editVisibility}
+                                        deleteVisible={deleteVisible}
+                                        setDeleteVisible={setDeleteVisible}
+                                        editFormStatus={editFormStatus}
+                                    />
+                                    {user.id === comment.userId && (
                                         <div>
-                                            {editFormStatus[comment.id] && (
-                                                <div>
-                                                    <EditCommentForm comment={comment} recipe={recipe} setEditFormStatus={setEditFormStatus} editFormStatus={editFormStatus} />
-                                                </div>
-                                            )}
+                                            <div>
+                                                {editFormStatus[comment.id] && (
+                                                    <div>
+                                                        <EditCommentForm comment={comment} recipe={recipe} setEditFormStatus={setEditFormStatus} editFormStatus={editFormStatus} />
+                                                    </div>
+                                                )}
+                                            </div>
                                         </div>
-                                    </div>
-                                )}
-                            </div>
-                        )
-                    })}
+                                    )}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
