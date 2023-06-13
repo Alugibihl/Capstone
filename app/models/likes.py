1b22ea8dfce9631f21dc7f65c79558c7fca5bf8b
alexl <alexandermlugbiihl@gmail.com>
2023-06-13 16:02:05 -0400

    render specifc update

--- app/models/likes.py
+++ app/models/likes.py
@@ -1,9 +1,11 @@
-from .db import db
+from .db import db, environment, add_prefix_for_prod, SCHEMA
 
 
 likes = db.Table(
     "likes",
     db.Model.metadata,
-    db.Column('users', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('recipes', db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
+    db.Column('users', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+    db.Column('recipes', db.Integer, db.ForeignKey(add_prefix_for_prod('recipes.id')), primary_key=True)
 )
+if environment == "production":
+    likes.schema = SCHEMA
